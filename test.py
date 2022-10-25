09c5cbb209515b0ef9daa97fe33a055970f65850
anthony.loupos@ibm.com <anthony.louposibm.com@MacBook-Pro-4.local>
2022-10-25 15:09:58 -0400

    update test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 import config
 import sys
+import time
 conf = config.get_config()
 
 connt = []
@@ -9,11 +10,19 @@ connt = []
 for k,v in conf['DATABASE']['CONNECTIONS'].items():
     connt.append((v['host'],v['port']))
 
+print (connt)
 auth_provider = PlainTextAuthProvider(username=conf['DATABASE']['USERNAME'], password=conf['DATABASE']['PASSWORD'])
 cluster = Cluster(connt,auth_provider=auth_provider)
 
-session = cluster.connect()
-
-print(session.execute("SELECT release_version FROM system.local").one())
+try:
+    session = cluster.connect()
+except:
+    pass
+try:
+    print(session.execute("SELECT release_version FROM system.local").one())
+except:
+    pass
+while True:
+    time.sleep(5)
 #session.set_keyspace('demo')
 #print(session.execute("SELECT * from customers").one())
