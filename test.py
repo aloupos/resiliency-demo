e1da319a7ac30131cf08b35aea49ec6ed8dac5b3
anthony.loupos@ibm.com <anthony.louposibm.com@MacBook-Pro-4.local>
2022-11-01 13:15:21 -0400

    implemented AddressTranslator to resolve potential issues connecting to cluster in containers.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+from cassandra.policies import AddressTranslator
 import config
 import sys
 import time
@@ -10,22 +11,40 @@ connt = []
 for k,v in conf['DATABASE']['CONNECTIONS'].items():
     connt.append((v['host'],v['port']))
 
-print (connt)
+#print (connt)
+# code from Rajesh
+class K8SAddressTranslator(AddressTranslator):
+
+    def translate(self, addr):
+
+        print('in translate(self, addr) method', type(addr), addr)
+        trans_addr = connt[0]
+        return trans_addr
+################################################################################
+
+
+k8s_translator = K8SAddressTranslator()
+
+
+
+
 auth_provider = PlainTextAuthProvider(username=conf['DATABASE']['USERNAME'], password=conf['DATABASE']['PASSWORD'])
-cluster = Cluster(connt,auth_provider=auth_provider)
+cluster = Cluster(connt,auth_provider=auth_provider,address_translator = k8s_translator)
 
 try:
     session = cluster.connect()
-
-except:
+except Exception as e:
+    print(e)
     pass
 try:
-    print(session.execute("SELECT release_version FROM system.local").one())
+    #print(session.execute("SELECT release_version FROM system.local").one())
+    print(session.execute("SELECT data_center, rack, host_id, release_version FROM system.local").all())
 except:
     pass
 while True:
     time.sleep(5)
     print (cluster.contact_points)
-    print(session.execute("SELECT release_version FROM system.local").one())
+    print(session.execute("SELECT data_center, rack, host_id, release_version FROM system.local").all())
+    #print(session.execute("SELECT release_version FROM system.local").one())
 #session.set_keyspace('demo')
 #print(session.execute("SELECT * from customers").one())
