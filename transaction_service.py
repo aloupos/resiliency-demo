c50e7c784ac39f7e86a2662a6bb506d42a8b45ca
Anthony Loupos <anthony.loupos@ibm.com>
2022-11-09 08:17:38 -0500

    exit on db error, kube restarts

--- transaction_service.py
+++ transaction_service.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
+from flask import Response
 import logging
+import os
 import json
 import collections
 from db_cassandra import session
@@ -23,6 +25,24 @@ class DecimalEncoder(json.JSONEncoder):
             return str(obj)
         return json.JSONEncoder.default(self, obj)
 
+@app.after_request
+def response_processor(response):
+    # Prepare all the local variables you need since the request context
+    # will be gone in the callback function
+
+    @response.call_on_close
+    def process_after_request():
+        # Do whatever is necessary here
+        print ("after")
+        print (response.status)
+        print (type(response.status))
+        if (response.status == '500 INTERNAL SERVER ERROR'):
+            print ("The server encountered an error, most likely due to the availability of dbs.  Exiting.")
+            os._exit(0)
+        pass
+
+    return response
+
 @app.route("/")
 def hello():
     return "ok"
@@ -38,7 +58,12 @@ def transactions():
 
 @app.route("/balance")
 def balance():
+    # try:
     q = session.execute('select id,account_type_id from account where customer_id = 2 ALLOW FILTERING')
+    # except Exception as e:
+    #     print (e)
+    #     return Response("{}", status=500, mimetype='application/json')
+
     # print (q[0])
     res = []
     for row in q:
